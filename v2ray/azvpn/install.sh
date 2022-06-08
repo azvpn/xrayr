6d3702c0478f3efbc10801499b7702d50c3e0b0d
azvpn cute <106140258+azvpn@users.noreply.github.com>
2022-06-08 09:30:23 +0700

    Update install.sh

--- v2ray/azvpn/install.sh
+++ v2ray/azvpn/install.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
 clear
-echo "          --------------------------------------------------------------------------------------------------------------------------------------------------"
+echo "---------------------------------------------------------------------------------------------------------------------------------"
 echo -e ""
-echo "          ███████████   ███████████   ███████████   ███████████   ███████████   ██       ██   ███████████       ██          ██   ██          ██   ██████████"
-echo "          ██       ██   ██       ██   ██            ██            ██       ██   ██       ██   ██       ██        ██        ██     ██        ██            ██"
-echo "          ██            ██       ██   ██            ██            ██       ██   ██       ██   ██                  ██      ██       ██      ██            ██"
-echo "          ██            ██       ██   ██            ██            ██       ██   ██       ██   ██                   ██    ██         ██    ██            ██"
-echo "          ██            ██       ██   ██            ██            ██       ██   ██       ██   ██                    ██  ██           ██  ██            ██"
-echo "          ███████████   ███████████   ███████████   ███████████   ██       ██   ███████████   ██                     ████             ████            ██"
-echo "                   ██   ██            ██            ██            ██       ██            ██   ██   ██████            ████              ██            ██"
-echo "                   ██   ██            ██            ██            ██       ██            ██   ██   ██  ██           ██  ██             ██           ██"
-echo "                   ██   ██            ██            ██            ██       ██            ██   ██       ██          ██    ██            ██          ██"
-echo "          ██       ██   ██            ██            ██            ██       ██            ██   ██       ██         ██      ██           ██         ██"
-echo "          ███████████   ██            ███████████   ███████████   ███████████            ██   ███████████   ██   ██        ██          ██         ██████████"
+echo "                          █████  ███████ ██    ██ ██████  ███    ██    ███    ███ ███████" 
+echo "                         ██   ██    ███  ██    ██ ██   ██ ████   ██    ████  ████ ██"      
+echo "                         ███████   ███   ██    ██ ██████  ██ ██  ██    ██ ████ ██ ████"█   
+echo "                         ██   ██  ███     ██  ██  ██      ██  ██ ██    ██  ██  ██ ██"      
+echo "                         ██   ██ ███████   ████   ██      ██   ████ ██ ██      ██ ███████" 
 echo -e ""
-echo "          --------------------------------------------------------------------------------------------------------------------------------------------------"
+echo "----------------------------------------------------------------------------------------------------------------------------------"
 echo -e ""
-echo "                                                 Quá Trình Cài Đặt XrayR Cho Máy Chủ Của Bạn Đang Diễn Ra !"
+echo "                          Quá Trình Cài Đặt XrayR Cho Máy Chủ Của Bạn Đang Diễn Ra !"
 echo -e ""
 red='\033[0;31m'
 green='\033[0;32m'
@@ -45,7 +39,7 @@ elif cat /proc/version | grep -Eqi "ubuntu"; then
 elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
     release="centos"
 else
-    echo -e "  Phiên Bản Không Hợp Lệ Vui Lòng Liên Hệ ADMIN SPEED4G.XYZ ${plain}\n" && exit 1
+    echo -e "  Phiên Bản Không Hợp Lệ Vui Lòng Liên Hệ ADMIN AZVPN.ME ${plain}\n" && exit 1
 fi
 
 arch=$(arch)
@@ -62,7 +56,7 @@ fi
 echo "  Tiến Trình: ${arch}"
 
 if [ "$(getconf WORD_BIT)" != '32' ] && [ "$(getconf LONG_BIT)" != '64' ] ; then
-    echo "  Phần Mềm Này Không Hỗ Trợ Hệ Thống 32-bit (x86), Vui Lòng Sử Dụng Hệ Thống 64-bit (x86_64), Nếu Phát Hiện Sai, Vui Lòng Liên Hệ ADMIN SPEED4G.XYZ"
+    echo "  Phần Mềm Này Không Hỗ Trợ Hệ Thống 32-bit (x86), Vui Lòng Sử Dụng Hệ Thống 64-bit (x86_64), Nếu Phát Hiện Sai, Vui Lòng Liên Hệ ADMIN AZVPN.ME"
     exit 2
 fi
 
@@ -126,26 +120,26 @@ install_XrayR() {
 	cd /usr/local/XrayR/
 
     if  [ $# == 0 ] ;then
-        bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/clear.sh)
-        last_version=$(curl -Ls "https://api.github.com/repos/Nghi235/xrayrr/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-	bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/clear.sh)
+        bash <(curl -Ls https://raw.githubusercontent.com/azvpn/status/main/clear.sh)
+        last_version=$(curl -Ls "https://api.github.com/repos/azvpn/xrayrr/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+	bash <(curl -Ls https://raw.githubusercontent.com/azvpn/status/main/clear.sh)
         if [[ ! -n "$last_version" ]]; then
             echo -e "  Không Phát Hiện Được Phiên Bản XrayR, Có Thể Đã Vượt Quá Giới Hạn Github API, Vui Lòng Thử Lại Sau Hoặc Chỉ Định Phiên Bản XrayR Để Cài Đặt $ theo cách thủ công{plain}"
             exit 1
         fi
         echo -e "  Đã Phát Hiện Phiên Bản Mới Nhất Của XrayR：${last_version}，Bắt Đầu Cài Đặt"
-	bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/clear.sh)
-        wget -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux.zip https://github.com/Nghi235/xrayrr/releases/download/${last_version}/XrayR-linux-${arch}.zip
-	bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/clear.sh)
+	bash <(curl -Ls https://raw.githubusercontent.com/azvpn/status/main/clear.sh)
+        wget -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux.zip https://github.com/azvpn/xrayrr/releases/download/${last_version}/XrayR-linux-${arch}.zip
+	bash <(curl -Ls https://raw.githubusercontent.com/azvpn/status/main/clear.sh)
         if [[ $? -ne 0 ]]; then
             echo -e "  Không Tải Xuống Được XrayR, Hãy Đảm Bảo Máy Chủ Của Bạn Có Thể Tải Xuống Tệp Github ${plain}"
             exit 1
         fi
     else
         last_version=$1
-	bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/clear.sh)
-        url="https://github.com/Nghi235/xrayrr/releases/download/${last_version}/XrayR-linux-${arch}.zip"
-	bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/clear.sh)
+	bash <(curl -Ls https://raw.githubusercontent.com/azvpn/status/main/clear.sh)
+        url="https://github.com/azvpn/xrayrr/releases/download/${last_version}/XrayR-linux-${arch}.zip"
+	bash <(curl -Ls https://raw.githubusercontent.com/azvpn/status/main/clear.sh)
         echo -e "  Bắt Đầu Cài Đặt XrayR v$1"
         wget -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux.zip ${url}
         if [[ $? -ne 0 ]]; then
@@ -153,13 +147,13 @@ install_XrayR() {
             exit 1
         fi
     fi
-bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/clear.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/azvpn/status/main/clear.sh)
     unzip XrayR-linux.zip
     rm XrayR-linux.zip -f
     chmod +x XrayR
     mkdir /etc/XrayR/ -p
     rm /etc/systemd/system/XrayR.service -f
-    file="https://raw.githubusercontent.com/Nghi235/xrayr/main/v2ray/speed4g/XrayR.service"
+    file="https://raw.githubusercontent.com/azvpn/xrayr/main/v2ray/azvpn/XrayR.service"
     wget -N --no-check-certificate -O /etc/systemd/system/XrayR.service ${file}
     #cp -f XrayR.service /etc/systemd/system/
     systemctl daemon-reload
@@ -172,14 +166,14 @@ bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/clear.sh)
     if [[ ! -f /etc/XrayR/config.yml ]]; then
         cp config.yml /etc/XrayR/
         echo -e ""
-        echo -e "  Nếu Không Biết Cấu Hình Vui Lòng Liên Hệ ADMIN SPEED4G.XYZ"
+        echo -e "  Nếu Không Biết Cấu Hình Vui Lòng Liên Hệ ADMIN AZVPN.ME"
     else
         systemctl start XrayR
         sleep 2
         check_status
         echo -e ""
         if [[ $? == 0 ]]; then
-            echo -e "  XrayR Khởi Động Thành Công ! (COPYRIGHT BY ADMIN SPEED4G.XYZ)${plain}"
+            echo -e "  XrayR Khởi Động Thành Công ! (COPYRIGHT BY ADMIN AZVPN.ME)${plain}"
         else
             echo -e "  XrayR Khởi Động Thất Bại, Vui Lòng Sử Dụng XrayR Log Để Kiểm Tra${plain}"
         fi
@@ -194,14 +188,14 @@ bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/clear.sh)
     if [[ ! -f /etc/XrayR/custom_outbound.json ]]; then
         cp custom_outbound.json /etc/XrayR/
     fi
-    bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/clear.sh)
-    curl -o /usr/bin/XrayR -Ls https://raw.githubusercontent.com/Nghi235/xrayr/main/v2ray/speed4g/XrayR.sh
+    bash <(curl -Ls https://raw.githubusercontent.com/azvpn/status/main/clear.sh)
+    curl -o /usr/bin/XrayR -Ls https://raw.githubusercontent.com/azvpn/xrayr/main/v2ray/azvpn/XrayR.sh
     chmod +x /usr/bin/XrayR
     ln -s /usr/bin/XrayR /usr/bin/xrayr # chữ thường tương thích
     chmod +x /usr/bin/xrayr
     echo -e ""
-    echo "------------[Nguyễn Nghị]------------"
-    echo "---------[ADMIN SPEED4G.XYZ]---------"
+    echo "------------[Nguyễn Mạnh Long]------------"
+    echo "-------------[ADMIN AZVPN.ME]-------------"
     echo "  Cách Sử Dụng Tập Lệnh Quản Lý XrayR"
     echo "---------------------------------------------------------"
     echo "  XrayR                      - Hiển Thị Menu Quản Lý"
@@ -226,4 +220,4 @@ echo -e "  Bắt Đầu Cài Đặt ${plain}"
 install_base
 install_acme
 install_XrayR $1
-bash <(curl -Ls https://raw.githubusercontent.com/Nghi235/status/main/nhapdomain.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/azvpn/status/main/nhapdomain.sh)
